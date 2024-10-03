9208a4eedcfc629d18008b190afcd83d731d800d
Matheus_Matta <matheuseduardo3004@gmail.com>
2024-10-03 14:26:42 -0300

    version 0.5

--- modulos/dashboard/urls.py
+++ modulos/dashboard/urls.py
@@ -6,6 +6,8 @@ urlpatterns = [
     
     path('dashboard/campaign', dashboard_campaign, name="dashboard_campaign"),
     path('dashboard/campaign/<int:campaign_id>', details_campaign, name="details_campaign"),
+    path('dashboard/campaign/encerrar/<int:campaign_id>', encerrar_campaign, name="encerrar_campaign"),
+    path('dashboard/campaign/del/<int:campaign_id>', delete_campaign, name="delete_campaign"),
 
     
 ]
--- modulos/dashboard/views/campaign.py
+++ modulos/dashboard/views/campaign.py
@@ -5,8 +5,8 @@ from django.contrib.auth.decorators import login_required
 import requests
 from datetime import datetime
 from calendar import monthrange
-
 import json
+
 
 @login_required
 def dashboard_campaign(request):
@@ -24,14 +24,16 @@ def dashboard_campaign(request):
             last_day = monthrange(today.year, today.month)[1]
             data_fim = today.replace(day=last_day).strftime('%Y-%m-%d')
 
-        api_url = f"http://186.216.60.62:8888/api/campaigns?dataInicio={data_inicio}&dataFim={data_fim}"
+        # Faz a requisição para a API
+        api_url = f"http://10.0.0.73:8888/api/campaigns?dataInicio={data_inicio}&dataFim={data_fim}"
         response = requests.get(api_url)
 
+        # Verifica o status da resposta da API
         if response.status_code == 200:
-            campaigns_data = response.json()
-            campaigns = json.loads(campaigns_data['campaigns'])
+            campaigns = response.json().get('campaigns', [])
 
-        total_numbers = 0 
+        # Inicializando as variáveis globais para os cálculos
+        total_numbers = 0
         total_numbers_now = 0
         campaign_total_now = 0
         total_success = 0
@@ -43,52 +45,59 @@ def dashboard_campaign(request):
         campaign_finaly = 0
         campaign_finaly_now = 0
 
+        # Itera sobre as campanhas e realiza os cálculos
         for campaign in campaigns:
-            total_numbers_value = campaign['fields'].get('total_numbers')
-            total_msg_success_value = campaign['fields'].get('send_success')
-            total_msg_erro_value = campaign['fields'].get('send_erro') 
-            total_responses += int(campaign['fields'].get('responses'))
-            
+            total_numbers_value = campaign.get('total_numbers')
+            total_msg_success_value = campaign.get('send_success')
+            total_msg_erro_value = campaign.get('send_error')
+            total_responses_value = campaign.get('response_count', 0)
+
+            # Acumula os dados globais
             total_numbers += int(total_numbers_value) if total_numbers_value else 0
             total_success += int(total_msg_success_value) if total_msg_success_value else 0
             total_erro += int(total_msg_erro_value) if total_msg_erro_value else 0
+            total_responses += total_responses_value
 
-            if campaign['fields'].get('status') == 'finalizado':
+            # Verifica se a campanha foi finalizada
+            if campaign.get('status') == 'finalizado':
                 campaign_finaly += 1
 
-            campaign['fields']['response_rate'] = (int(campaign['fields']['responses']) / total_success) * 100 if total_success > 0 else 0
+            # Calcula a taxa de resposta individual da campanha
+            campaign_success = int(total_msg_success_value) if total_msg_success_value else 0
+            campaign_response_rate = (total_responses_value / campaign_success) * 100 if campaign_success > 0 else 0
+            campaign['response_rate'] = round(campaign_response_rate, 1)
 
-            start_date_str = campaign['fields'].get('start_date')
+            # Formatando datas
+            start_date_str = campaign.get('start_date')
             if start_date_str:
-                start_date_obj = datetime.strptime(start_date_str, '%Y-%m-%dT%H:%M:%S.%fZ')
-                campaign['fields']['start_date_formatted'] = start_date_obj.strftime('%Y-%m-%d %H:%M')
+                start_date_obj = datetime.strptime(start_date_str, '%Y-%m-%dT%H:%M:%S.%f')
+                campaign['start_date_formatted'] = start_date_obj.strftime('%Y-%m-%d %H:%M')
             else:
-                campaign['fields']['start_date_formatted'] = 'N/A'
+                campaign['start_date_formatted'] = 'N/A'
 
-            end_date_str = campaign['fields'].get('end_date')
+            end_date_str = campaign.get('end_date')
             if end_date_str:
-                end_date_obj = datetime.strptime(end_date_str, '%Y-%m-%dT%H:%M:%S.%fZ')
-                campaign['fields']['end_date_formatted'] = end_date_obj.strftime('%Y-%m-%d %H:%M')
+                end_date_obj = datetime.strptime(end_date_str, '%Y-%m-%dT%H:%M:%S.%f')
+                campaign['end_date_formatted'] = end_date_obj.strftime('%Y-%m-%d %H:%M')
             else:
-                campaign['fields']['end_date_formatted'] = 'N/A'
+                campaign['end_date_formatted'] = 'N/A'
 
-            start_date = datetime.strptime(campaign['fields']['start_date'], '%Y-%m-%dT%H:%M:%S.%fZ').date()
+            # Verifica se a campanha foi iniciada hoje
+            start_date = datetime.strptime(campaign.get('start_date'), '%Y-%m-%dT%H:%M:%S.%f').date()
             if start_date == datetime.now().date():
                 total_numbers_now += int(total_numbers_value) if total_numbers_value else 0
                 total_erro_now += int(total_msg_erro_value) if total_msg_erro_value else 0
                 total_success_now += int(total_msg_success_value) if total_msg_success_value else 0
-                total_responses_now += int(campaign['fields'].get('responses'))
+                total_responses_now += campaign.get('response_count', 0)
                 campaign_total_now += 1
-                if campaign['fields'].get('status') == 'finalizado':
+                if campaign.get('status') == 'finalizado':
                     campaign_finaly_now += 1
 
-        response_rate = 0
-        response_rate_now = 0
-        if total_success > 0:
-            response_rate = (total_responses / total_success) * 100
-        if total_success_now > 0:
-            response_rate_now = (total_responses_now / total_success_now) * 100
-        
+        # Calcula a taxa de resposta global para o período filtrado
+        response_rate = (total_responses / total_success) * 100 if total_success > 0 else 0
+        response_rate_now = (total_responses_now / total_success_now) * 100 if total_success_now > 0 else 0
+
+        # Renderiza os dados para o template
         return render(request, 'dashboard/dashboard-campaign.html', {
             'campaigns': campaigns,
             'total_numbers': total_numbers,
@@ -99,8 +108,8 @@ def dashboard_campaign(request):
             'total_success_now': total_success_now,
             "total_erro": total_erro,
             'total_erro_now': total_erro_now,
-            'response_rate': response_rate,
-            'response_rate_now': response_rate_now,
+            'response_rate': f"{response_rate:.2f}",
+            'response_rate_now': f"{response_rate_now:.2f}",
             'campaign_finaly': campaign_finaly,
             'campaign_finaly_now': campaign_finaly_now
         })
@@ -113,26 +122,31 @@ def dashboard_campaign(request):
 @login_required
 def details_campaign(request, campaign_id):
     try:
-        api_url = f"http://186.216.60.62:8888/api/campaigns/{campaign_id}"
+        # Faz a requisição para a API
+        api_url = f"http://10.0.0.73:8888/api/campaigns/{campaign_id}"
         response = requests.get(api_url)
         campaign_response = response.json()
-        print(campaign_response)
-        if response.status_code != 200 and response.status_code != 201:
-            print("[details_campaign] ERROR: campanha não existe")
-            messages.error(request, response.text)
+
+        # Verifica o status da resposta da API
+        if response.status_code != 200:
+            messages.error(request, "[details_campaign] ERROR: campanha não existe")
             return redirect('dashboard_campaign')
 
-        # Extraindo e convertendo a string JSON da campanha
+        # Extraindo os dados da campanha
         campaign_data_str = campaign_response.get('campaign', '[]')
         campaign_list = json.loads(campaign_data_str)
         if not campaign_list:
             raise Exception("[details_campaign] ERROR: Dados da campanha não encontrados.")
         campaign_obj = campaign_list[0]  # Pega o primeiro elemento da lista
         campaign_fields = campaign_obj.get('fields', {})  # Acessa o 'fields' da campanha
+        campaign_fields['id'] = campaign_obj['pk']
 
-        # Extraindo e convertendo a string JSON dos logs
+        # Extraindo e convertendo os logs
         logs_data_str = campaign_response.get('logs', '[]')
         logs = json.loads(logs_data_str)
+
+        # Contagem de respostas separada
+        responses = campaign_response.get('responses', 0)
 
         # Inicializando os arrays de contagem para cada hora do dia
         array_horas_sucesso = [0] * 24  # Mensagens de sucesso
@@ -142,51 +156,90 @@ def details_campaign(request, campaign_id):
         for log in logs:
             fields = log.get('fields', {})
             criado = fields.get('created_at')
-            status = fields.get('status')  # Verifica o status da mensagem
+            status = fields.get('status')
 
             if criado:
                 # Converte a string para um objeto datetime
-                data_criacao = datetime.strptime(criado, '%Y-%m-%dT%H:%M:%S.%fZ')
+                data_criacao = datetime.strptime(criado, '%Y-%m-%dT%H:%M:%S.%f')
                 hora = data_criacao.hour  # Obtém a hora do log
 
+                # Formata a data de criação
                 start_date_str = fields.get('created_at')
-                if start_date_str:  # Verifica se existe uma data antes de converter
+                if start_date_str:
                     try:
-                        start_date_obj = datetime.strptime(start_date_str, '%Y-%m-%dT%H:%M:%S.%fZ')
+                        start_date_obj = datetime.strptime(start_date_str, '%Y-%m-%dT%H:%M:%S.%f')
                         log['fields']['start_date_formatted'] = start_date_obj.strftime('%Y-%m-%d %H:%M')
                     except ValueError:
-                        # Caso o formato da data não corresponda, tratamos o erro aqui
                         log['fields']['start_date_formatted'] = 'Formato de data inválido'
                 else:
                     log['fields']['start_date_formatted'] = 'Data não disponível'
 
                 # Atualiza o array correspondente com base no status
                 if status == 'sucesso':
-                    array_horas_sucesso[hora] += 1  # Incrementa o índice para sucesso
+                    array_horas_sucesso[hora] += 1
                 elif status == 'erro':
-                    array_horas_erro[hora] += 1  # Incrementa o índice para erro
+                    array_horas_erro[hora] += 1
 
         # Métricas da campanha
-        total_numbers = int(campaign_fields.get('total_numbers') if campaign_fields.get('total_numbers') else 0)
-        total_success = int(campaign_fields.get('send_success') if campaign_fields.get('send_success') else 0)
-        total_erro = int(campaign_fields.get('send_erro') if campaign_fields.get('send_erro') else 0)
-        responses = int(campaign_fields.get('responses') if campaign_fields.get('responses') else 0)
+        total_numbers = int(campaign_fields.get('total_numbers', 0) or 0)  # Garante que o valor não seja None
+        total_success = int(campaign_fields.get('send_success', 0) or 0)  # Mesma coisa para send_success
+        total_erro = int(campaign_fields.get('send_erro', 0) or 0)  # Mesma coisa para send_erro
 
-        # Calcula a taxa de resposta
+        # Calcula a taxa de resposta com base no total de sucessos
         response_rate = (responses / total_success) * 100 if total_success > 0 else 0
 
+        # Renderiza os dados para o template
         return render(request, 'details/details-campaign.html', {
             'campaign': campaign_fields,
             'total_numbers': total_numbers,
             'total_success': total_success,
             "total_erro": total_erro,
-            'response_rate': response_rate,
+            'response_rate': f"{response_rate:.2f}",
             'msg_sucess': array_horas_sucesso,
             'msg_erro': array_horas_erro,
             'logs': logs,
         })
 
     except Exception as e:
-        print(f'[details_campaign] ERROR exception: {e}')
+        print(f'[details_campaign] ERROR: {e}')
         messages.error(request, f'[details_campaign] ERROR: {str(e)}')
         return redirect('dashboard_campaign')
+    
+@login_required
+def encerrar_campaign(request, campaign_id):
+    try:
+        api_url = f"http://10.0.0.73:8888/api/campaigns/exit/{campaign_id}"
+        response = requests.post(api_url)
+        campaign_response = response.json()
+
+        # Verifica o status da resposta da API
+        if response.status_code != 200 or response.status_code != 201:
+            print(f'[encerra_campanha] error {campaign_response}')
+            messages.error(request, "[details_campaign] ERROR: campanha não existe")
+            return redirect('dashboard_campaign')
+        
+        messages.success(request, "Campanha encerrada com sucesso.")
+        return redirect('dashboard_campaign')
+    except Exception as e:
+        print(f'[encerra_campanha] error {e}')
+        messages.error(request, f'[encerra_campanha] ERROR: {str(e)}')
+        redirect('dashboard_campaign')
+
+def delete_campaign(request, campaign_id):
+    try:
+        api_url = f"http://10.0.0.73:8888/api/campaigns/del/{campaign_id}"
+        response = requests.post(api_url)
+        campaign_response = response.json()
+
+        # Verifica o status da resposta da API
+        if response.status_code != 200 or response.status_code != 201:
+            print(f'[encerra_campanha] error {campaign_response}')
+            messages.error(request, "[details_campaign] ERROR: campanha não existe")
+            return redirect('dashboard_campaign')
+        
+        messages.success(request, "Campanha excluida com sucesso!")
+        return redirect('dashboard_campaign')
+    except Exception as e:
+        print(f'[encerra_campanha] error {e}')
+        messages.error(request, f'[encerra_campanha] ERROR: {str(e)}')
+        redirect('dashboard_campaign')
